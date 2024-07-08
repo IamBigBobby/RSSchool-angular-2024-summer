93c1ff47c8e2d0fef8f44c8af411ce480c055972
Denis Svetleishii <denissvetleishii@outlook.com>
2024-07-08 10:58:50 +0400

    feat: get value from input search

--- youtube-client-app/src/app/home/header/search-field/search-field.component.ts
+++ youtube-client-app/src/app/home/header/search-field/search-field.component.ts
@@ -8,12 +8,16 @@ import { Component } from '@angular/core';
     <section class="search-field">
       <img src="/assets/logo.svg" alt="logo" />
       <form>
-        <input type="text" placeholder="What are you want to find out?" />
-        <button class="primary" type="button">Search</button>
+        <input type="text" placeholder="What are you want to find out?" #filter />
+        <button class="primary" type="button" (click)="getFilterValue(filter.value)">Search</button>
       </form>
       <img src="/assets/search_settings.svg" alt="search_settings" />
     </section>
   `,
   styleUrl: './search-field.component.scss',
 })
-export class SearchFieldComponent {}
+export class SearchFieldComponent {
+  getFilterValue(word: string) {
+    console.log('filter value', word);
+  }
+}
