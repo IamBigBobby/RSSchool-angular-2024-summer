54366b993d99200db941377ccf917777b80d96fc
Denis Svetleishii <denissvetleishii@outlook.com>
2024-07-18 16:34:37 +0400

    refactor: move nested objects into separate interfaces

--- src/app/you-tube-interface.ts
+++ src/app/you-tube-interface.ts
@@ -2,63 +2,59 @@ export interface VideoItem {
   kind: string;
   etag: string;
   id: string;
-  snippet: {
-    publishedAt: string;
-    channelId: string;
-    title: string;
-    description: string;
-    thumbnails: {
-      default: {
-        url: string;
-        width: number;
-        height: number;
-      };
-      medium: {
-        url: string;
-        width: number;
-        height: number;
-      };
-      high: {
-        url: string;
-        width: number;
-        height: number;
-      };
-      standard: {
-        url: string;
-        width: number;
-        height: number;
-      };
-      maxres: {
-        url: string;
-        width: number;
-        height: number;
-      };
-    };
-    channelTitle: string;
-    tags: string[];
-    categoryId: string;
-    liveBroadcastContent: string;
-    localized: {
-      title: string;
-      description: string;
-    };
-    defaultAudioLanguage: string;
-  };
-  statistics: {
-    viewCount: string;
-    likeCount: string;
-    dislikeCount: string;
-    favoriteCount: string;
-    commentCount: string;
-  };
+  snippet: Snippet;
+  statistics: Statistics;
 }
 
 export interface YouTubeInterface {
   kind: string;
   etag: string;
-  pageInfo: {
-    totalResults: number;
-    resultsPerPage: number;
-  };
+  pageInfo: PageInfo;
   items: VideoItem[];
+}
+
+interface Thumbnail {
+  url: string;
+  width: number;
+  height: number;
+}
+
+interface Thumbnails {
+  default: Thumbnail;
+  medium: Thumbnail;
+  high: Thumbnail;
+  standard: Thumbnail;
+  maxres: Thumbnail;
+}
+
+interface Localized {
+  title: string;
+  description: string;
+}
+
+interface Snippet {
+  publishedAt: string;
+  channelId: string;
+  title: string;
+  description: string;
+  thumbnails: Thumbnails;
+  channelTitle: string;
+  tags: string[];
+  categoryId: string;
+  liveBroadcastContent: string;
+  localized: Localized;
+  defaultAudioLanguage: string;
+}
+
+interface Statistics {
+  viewCount: string;
+  likeCount: string;
+  dislikeCount: string;
+  favoriteCount: string;
+  commentCount: string;
+}
+
+interface PageInfo {
+  totalResults: number;
+  resultsPerPage: number;
 }
