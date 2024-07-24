c8c28db3a895bdd228890ac0089cc8ba76b3850c
Denis Svetleishii <denissvetleishii@outlook.com>
2024-07-24 18:52:11 +0400

    feat: add base functionality into LoginComponent

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from '../shared/components/button/button.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
-  template: ` <p>login works!</p> `,
+  imports: [ButtonComponent],
+  template: `
+    <form class="login">
+      <input
+        class="login__input-user"
+        type="text"
+        placeholder="Enter your login"
+        #inputUser
+      />
+      <input
+        class="login__input-password "
+        type="text"
+        placeholder="Enter your password"
+        #inputPassword
+      />
+      <app-button
+        class="login__submit"
+        (click)="submitUser(inputUser.value, inputPassword.value)"
+        >Login</app-button
+      >
+    </form>
+  `,
   styleUrl: './login.component.scss',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  protected submitUser(user: string, password: string) {
+    console.log(user, password);
+  }
+}
