1dac906d7b02846845ebc7c653ddbf378365a2db
Denis Svetleishii <denissvetleishii@outlook.com>
2024-07-24 18:09:48 +0400

    feat: add page note found component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { MainContentComponent } from './home/main/main-content.component';
 import { DetailedPageComponent } from './detailed/detailed-page.component';
 import { LoginComponent } from './login/login.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 export const routes: Routes = [
   { path: '', component: MainContentComponent, title: 'Main page' },
@@ -11,4 +12,5 @@ export const routes: Routes = [
     title: 'Detailed page',
   },
   { path: 'login-page', component: LoginComponent, title: 'Login page' },
+  { path: '**', component: PageNotFoundComponent, title: 'Page not found' },
 ];
