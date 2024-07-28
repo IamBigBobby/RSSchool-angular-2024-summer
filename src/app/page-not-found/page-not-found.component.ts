ecb1476bc8144081f159fa798cfc09d125ec330d
Denis Svetleishii <denissvetleishii@outlook.com>
2024-07-28 18:36:31 +0400

    feat: add styles for 404 page

--- src/app/page-not-found/page-not-found.component.ts
+++ src/app/page-not-found/page-not-found.component.ts
@@ -1,10 +1,23 @@
+import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-page-not-found',
   standalone: true,
-  imports: [],
-  template: ` <p>page-not-found works!</p> `,
+  imports: [NgOptimizedImage],
+  template: `
+    <div class="page-not-found__wrapper">
+      <img
+        class="page-not-found__img"
+        ngSrc="assets/404.svg"
+        width="100"
+        height="100"
+        priority
+        alt="404"
+      />
+      <div class="page-not-found__notification">Sorry, smth went wrong</div>
+    </div>
+  `,
   styleUrl: './page-not-found.component.scss',
 })
 export class PageNotFoundComponent {}
