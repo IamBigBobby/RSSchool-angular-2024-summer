57bf04b9794549a2dc7ec2fce1c9125548a01d6b
Denis Svetleishii <denissvetleishii@outlook.com>
2024-08-01 19:21:25 +0400

    feat: add reactive form to login component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,25 @@
 import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { ButtonComponent } from '../shared/components/button/button.component';
 import { LoginService } from './services/login.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ButtonComponent],
+  imports: [ButtonComponent, ReactiveFormsModule],
   template: `
     <div class="login__wrapper">
-      <form class="login">
+      <form
+        class="login"
+        [formGroup]="loginForm"
+        (ngSubmit)="
+          submitUser(
+            this.loginForm.value.login ?? '',
+            this.loginForm.value.password ?? ''
+          )
+        "
+      >
         <h2 class="login__title">Login</h2>
         <label for="user-input" class="login__label-user">Username</label>
         <input
@@ -17,7 +27,7 @@ import { LoginService } from './services/login.service';
           class="login__input-user"
           type="text"
           placeholder="Enter your login"
-          #inputUser
+          formControlName="login"
         />
         <label for="password-input" class="login__label-password"
           >Password</label
@@ -27,31 +37,26 @@ import { LoginService } from './services/login.service';
           class="login__input-password"
           type="password"
           placeholder="Enter your password"
-          #inputPassword
+          formControlName="password"
         />
-        <app-button
-          class="login__submit"
-          (click)="submitUser(inputUser.value, inputPassword.value)"
-          >Login</app-button
-        >
+        <app-button class="login__submit" type="submit">Login</app-button>
       </form>
     </div>
   `,
   styleUrl: './login.component.scss',
 })
 export class LoginComponent {
-  public user?: string;
-
-  public password?: string;
-
   private loginService = inject(LoginService);
 
   private router = inject(Router);
 
-  protected submitUser(user: string, password: string) {
-    this.user = user;
-    this.password = password;
+  loginForm = new FormGroup({
+    login: new FormControl(''),
+    password: new FormControl(''),
+  });
 
+  protected submitUser(user: string, password: string) {
+    console.log('user: ', user, 'password: ', password);
     this.loginService.login();
     this.router.navigate(['']);
   }
--- src/app/shared/components/button/button.component.ts
+++ src/app/shared/components/button/button.component.ts
@@ -1,17 +1,19 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-button',
   standalone: true,
   imports: [],
   template: `
-    <button class="custom-button" type="button" (click)="onButtonClick()">
+    <button class="custom-button" [type]="type" (click)="onButtonClick()">
       <ng-content></ng-content>
     </button>
   `,
-  styleUrl: './button.component.scss',
+  styleUrls: ['./button.component.scss'],
 })
 export class ButtonComponent {
+  @Input() type: 'button' | 'submit' = 'button';
+
   @Output() clicked = new EventEmitter<void>();
 
   onButtonClick() {
