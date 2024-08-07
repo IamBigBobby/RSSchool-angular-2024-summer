c9505c81457535c05ccd9eba64ede7477c588078
Denis Svetleishii <denissvetleishii@outlook.com>
2024-08-07 14:47:41 +0400

    feat: add lazy loading for componets

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,21 +1,28 @@
 import { Routes } from '@angular/router';
-import { MainContentComponent } from './home/main/main-content.component';
-import { DetailedPageComponent } from './detailed/detailed-page.component';
-import { LoginComponent } from './login/login.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { authGuard } from './login/guards/auth.guard';
 
 export const routes: Routes = [
   {
     path: '',
-    component: MainContentComponent,
+    loadComponent: () =>
+      import('./home/main/main-content.component').then(
+        (m) => m.MainContentComponent,
+      ),
     canActivate: [authGuard],
   },
   {
     path: 'detailed-page/:id',
-    component: DetailedPageComponent,
+    loadComponent: () =>
+      import('./detailed/detailed-page.component').then(
+        (m) => m.DetailedPageComponent,
+      ),
     canActivate: [authGuard],
   },
-  { path: 'login-page', component: LoginComponent },
+  {
+    path: 'login-page',
+    loadComponent: () =>
+      import('./login/login.component').then((m) => m.LoginComponent),
+  },
   { path: '**', component: PageNotFoundComponent },
 ];
