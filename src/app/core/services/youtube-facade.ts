123f0c8ce0287b0d14b3ab41bb0678fd6ebaf042
Denis Svetleishii <denissvetleishii@outlook.com>
2024-09-30 15:58:31 +0400

    feat: add signal to API and appearing data into template with using signal

--- src/app/core/services/youtube-facade.ts
+++ src/app/core/services/youtube-facade.ts
@@ -1,19 +1,18 @@
-import { inject, Injectable, signal } from "@angular/core";
-import { VideoItem } from "./you-tube-interface";
-import { YoutubeService } from "./youtube-service.service";
+import { inject, Injectable, signal } from '@angular/core';
+import { VideoItem } from './you-tube-interface';
+import { YoutubeService } from './youtube-service.service';
 
 @Injectable({
-    providedIn: 'root',
+  providedIn: 'root',
 })
+export class YoutubeFacadeService {
+  private youtubeService = inject(YoutubeService);
 
-export class YoutubeFacadeService{
-    
-    private youtubeService = inject(YoutubeService)
-    
-    public detailedVideoSignal = signal<VideoItem | null>(null);
+  public detailedVideoSignal = signal<VideoItem | null>(null);
 
-    public async fetchGetDetaledVideo(id: string): Promise <void>{
-        const getDetailedVideoResponse = await this.youtubeService.getDetailedVideo(id);
-        this.detailedVideoSignal.set(getDetailedVideoResponse)
-    }
+  public async fetchGetDetaledVideo(id: string): Promise<void> {
+    const getDetailedVideoResponse =
+      await this.youtubeService.getDetailedVideo(id);
+    this.detailedVideoSignal.set(getDetailedVideoResponse);
+  }
 }
